f0f4031de144d9aec8a44cf9c604b407d24c5ac7
Thomas Vu <shuhari@macbook.local>
2019-11-29 23:34:12 -0700

    a

---
+++ learnflask/apple/__init__.py
@@ -0,0 +1,6 @@
+fruit = 'tasty'
+num = 69
+things = [fruit, num]
+print('my name is:', __name__)
+
+from apple import fruit
---
+++ learnflask/test.py
@@ -0,0 +1,7 @@
+from apple import num
+#print(apple)
+#print(apple.fruit)
+#print(apple.num)
+#print(apple.things)
+print(num)
+print(fruit)
